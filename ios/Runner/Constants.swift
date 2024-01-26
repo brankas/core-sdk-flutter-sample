91a95d5f5298debf5eb345d1d8238cd797d0535c
Ejay Torres <ejay.torres@brank.as>
2024-01-26 15:25:22 +0800

    Updated Direct Tap SDK to v4.2.1 and Direct Tap Framework to v4.2.0

---
+++ ios/Runner/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Runner
+//
+//  Created by Ejay Torres on 9/28/22.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = ""
+    static let DESTINATION_ACCOUNT_ID = ""
+}
